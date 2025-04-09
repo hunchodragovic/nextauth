367334e9b08dbd6f55ab1e68ea76c3f6fe567fd8
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-04-10 00:34:54 +0100

    nextAuth again

---
+++ app/lib/nextAuth.ts
@@ -0,0 +1,17 @@
+import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: (process.env.GOOGLE_CLIENT_ID as string) || "",
+      clientSecret: (process.env.GOOGLE_CLIENT_SECRET as string) || "",
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 1 * 24 * 60 * 60, // 1 day
+  },
+  jwt: {},
+  callbacks: {},
+  secret: process.env.NEXTAUTH_SECRET,
+};
