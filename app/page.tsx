40a7415d3f9b470128cdc9fde6310e024f316b9e
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-04-10 00:41:27 +0100

    authOptions

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "./lib/nextAuth";
+
 const page = () => {
+  const session = getServerSession(authOptions);
   return <div>page</div>;
 };
 
